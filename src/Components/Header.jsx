665c3b7c2a02c03f5452b5c8148585fe68a6ecdf
Laiba0310Ashfaq <laiba.ashfaq0310@gmail.com>
2023-05-22 19:25:24 +0500

    Normalize line endings

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../CSS/Header.css'
+
+function Header() {
+  return (
+    <header>
+      
+      <h1>Magic Quote Generator</h1>
+      
+    </header>
+  );
+}
+
+export default Header;
